837027ad951865e5c423d8391168e9dd85eb0b91
Leon <nightfox500@gmail.com>
2019-09-10 08:56:09 +0200

    Update WebSocket.swift
    Updated file to latest swift version

--- Source/WebSocket.swift
+++ Source/WebSocket.swift
@@ -716,20 +716,20 @@ private class InnerWebSocket: Hashable {
                 switch frame.code {
                 case .text:
                     fire {
-                        self.event.message(data: frame.utf8.text)
+                        self.event.message(frame.utf8.text)
                         self.eventDelegate?.webSocketMessageText?(frame.utf8.text)
                     }
                 case .binary:
                     fire {
                         switch self.binaryType {
                         case .uInt8Array:
-                            self.event.message(data: frame.payload.array)
+                            self.event.message(frame.payload.array)
                         case .nsData:
-                            self.event.message(data: frame.payload.nsdata)
+                            self.event.message(frame.payload.nsdata)
                             // The WebSocketDelegate is necessary to add Objective-C compability and it is only possible to send binary data with NSData.
                             self.eventDelegate?.webSocketMessageData?(frame.payload.nsdata)
                         case .uInt8UnsafeBufferPointer:
-                            self.event.message(data: frame.payload.buffer)
+                            self.event.message(frame.payload.buffer)
                         }
                     }
                 case .ping:
@@ -742,11 +742,11 @@ private class InnerWebSocket: Hashable {
                     fire {
                         switch self.binaryType {
                         case .uInt8Array:
-                            self.event.pong(data: frame.payload.array)
+                            self.event.pong(frame.payload.array)
                         case .nsData:
-                            self.event.pong(data: frame.payload.nsdata)
+                            self.event.pong(frame.payload.nsdata)
                         case .uInt8UnsafeBufferPointer:
-                            self.event.pong(data: frame.payload.buffer)
+                            self.event.pong(frame.payload.buffer)
                         }
                         self.eventDelegate?.webSocketPong?()
                     }
@@ -967,8 +967,8 @@ private class InnerWebSocket: Hashable {
     }
 
     func closeConn() {
-        rd.remove(from: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
-        wr.remove(from: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+        rd.remove(from: RunLoop.main, forMode: RunLoop.Mode.default)
+        wr.remove(from: RunLoop.main, forMode: RunLoop.Mode.default)
         rd.delegate = nil
         wr.delegate = nil
         rd.close()
@@ -1024,7 +1024,7 @@ private class InnerWebSocket: Hashable {
 			security = .none
 		}
 
-		var path = CFURLCopyPath(req.url! as CFURL!) as String
+        var path = CFURLCopyPath(req.url! as CFURL?) as String
         if path == "" {
             path = "/"
         }
@@ -1058,7 +1058,7 @@ private class InnerWebSocket: Hashable {
         var (rdo, wro) : (InputStream?, OutputStream?)
         var readStream:  Unmanaged<CFReadStream>?
         var writeStream: Unmanaged<CFWriteStream>?
-        CFStreamCreatePairWithSocketToHost(nil, addr[0] as CFString!, UInt32(Int(addr[1])!), &readStream, &writeStream);
+        CFStreamCreatePairWithSocketToHost(nil, addr[0] as CFString?, UInt32(Int(addr[1])!), &readStream, &writeStream);
         rdo = readStream!.takeRetainedValue()
         wro = writeStream!.takeRetainedValue()
         (rd, wr) = (rdo!, wro!)
@@ -1081,14 +1081,14 @@ private class InnerWebSocket: Hashable {
             wr.setProperty(StreamNetworkServiceTypeValue.voice.rawValue, forKey: Stream.PropertyKey.networkServiceType)
         }
         if allowSelfSignedSSL {
-            let prop: Dictionary<NSObject,NSObject> = [kCFStreamSSLPeerName: kCFNull, kCFStreamSSLValidatesCertificateChain: NSNumber(value: false)]
-            rd.setProperty(prop, forKey: Stream.PropertyKey(rawValue: kCFStreamPropertySSLSettings as String as String))
-            wr.setProperty(prop, forKey: Stream.PropertyKey(rawValue: kCFStreamPropertySSLSettings as String as String))
+//            let prop: Dictionary<NSObject,NSObject> = [kCFStreamSSLPeerName: kCFNull, kCFStreamSSLValidatesCertificateChain: NSNumber(value: false)]
+//            rd.setProperty(prop, forKey: Stream.PropertyKey(rawValue: kCFStreamPropertySSLSettings as String as String))
+//            wr.setProperty(prop, forKey: Stream.PropertyKey(rawValue: kCFStreamPropertySSLSettings as String as String))
         }
         rd.delegate = delegate
         wr.delegate = delegate
-        rd.schedule(in: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
-        wr.schedule(in: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+        rd.schedule(in: RunLoop.main, forMode: RunLoop.Mode.default)
+        wr.schedule(in: RunLoop.main, forMode: RunLoop.Mode.default)
         rd.open()
         wr.open()
         try write(header, length: header.count)
@@ -1492,7 +1492,7 @@ private class InnerWebSocket: Hashable {
     func close(_ code : Int = 1000, reason : String = "Normal Closure") {
         let f = Frame()
         f.code = .close
-        f.statusCode = UInt16(truncatingBitPattern: code)
+        f.statusCode = UInt16(code)
         f.utf8.text = reason
         sendFrame(f)
     }
@@ -1650,7 +1650,7 @@ open class WebSocket: NSObject {
     fileprivate var ws: InnerWebSocket
     fileprivate var id = manager.nextId()
     fileprivate var opened: Bool
-    open override var hashValue: Int { return id }
+    open override var hash: Int { return id }
     /// Create a WebSocket connection to a URL; this should be the URL to which the WebSocket server will respond.
     public convenience init(_ url: String){
         self.init(request: URLRequest(url: URL(string: url)!), subProtocols: [])
